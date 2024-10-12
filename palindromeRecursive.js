ec5583794b9c3e80341e362b3188bcbb8c6195ad
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-10-12 11:27:46 +1100

    Use recursive function to solve palindrome problem.

---
+++ palindromeRecursive.js
@@ -0,0 +1,21 @@
+// Write a recursive function called isPalindrome which returns true if the
+// string passed to it is a palindrome. Otherwise it returns false
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+const isPalindrome = (str) => {
+  if (str.length <= 1) return true;
+
+  if (str[0] !== str[str.length - 1]) return false;
+
+  return isPalindrome(str.slice(1, -1));
+};
+
+console.log(isPalindrome("awesome"));
+console.log(isPalindrome("foobar"));
+console.log(isPalindrome("tacocat"));
+console.log(isPalindrome("amanaplanacanalpanama"));
